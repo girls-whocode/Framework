198b1c75fff8c1007685669cdfbe3aff5fb99fc9
Jessica Brown <jessica@jbrowns.com>
2017-07-17 08:24:00 -0400

    Update ver 0.1 Beta (Build 9) plg_system examples

--- index.php
+++ index.php
@@ -13,4 +13,6 @@
 
 # plg_system
 	echo 'echo clean_number(\'4f32k91025\'); <span style="color: orange; font-style: italic"># will output '.clean_number('4f32k91025').'</span>'.BRN;
-	echo 'echo decimal_to_fraction(\'.125\'); <span style="color: orange; font-style: italic"># will output </span>'.decimal_to_fraction ('.125').'</span>'.BRN;
+	echo 'echo decimal_to_fraction(\'.125\'); <span style="color: orange; font-style: italic"># will output '.decimal_to_fraction ('.125').'</span>'.BRN;
+	echo 'echo fraction_to_decimal(\'1/8\'); <span style="color: orange; font-style: italic"># will output '.fraction_to_decimal ('1/8').'</span>'.BRN;
+	echo 'echo is_whole_number(\'1/8\'); <span style="color: orange; font-style: italic"># will output '.is_whole_number (45.21).'</span>'.BRN;
