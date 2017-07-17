35767f5b10cf000388cc49b14d2ca39a0d11f46f
Jessica Brown <jessica@jbrowns.com>
2017-07-17 07:14:32 -0400

    Update ver 0.1 Beta (Build 6) Updated PHP Module testing

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 	define('BRN', "<br />\n");
 
 # plg_visitor
-	echo "You are using ".browserName." ver ".browserVer." on ".platformFamily." ".(is64bit ? "64 bit" : "32 bit")." from ".getIP();
+	echo "You are using ".browserName." ver ".browserVer." on ".platformFamily." ".(is64bit ? "64 bit" : "32 bit")." from ".getIP().BRN;
 
 # plg_dump
 	$arr = array("Defined Variables"=>get_defined_vars(), "Defined Constants"=>get_defined_constants());
-	echo dump($arr, TRUE, TRUE);
+	echo dump($arr, TRUE, TRUE).BRN;
 
 # plg_system
 	echo 'echo clean_number(\'4f32k91025\'); will output '.clean_number('4f32k91025').BRN;
